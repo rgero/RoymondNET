a6b064ab1f0e86b1c2b09984eacdd7d2a926308c
Roy Gero <roy.gero.ii@gmail.com>
2016-10-09 21:28:39 -0400

    Updated the results table just to print the variables. Implemented the get full names function and also fixed the getList function so it doesn't return unicode variables

--- PenaltyTracker/views.py
+++ PenaltyTracker/views.py
@@ -33,24 +33,64 @@ def print_results(request):
   if submittedForm.is_valid():
     #Get the raw data
     cleanedData = submittedForm.cleaned_data
-    playerName = cleanedData["playerName"]
-    homeTeams = cleanedData["homeTeam"]
-    awayTeams = cleanedData["awayTeam"]
+    playerNames = getList(cleanedData["playerName"])
+    homeTeams = getFullNames(cleanedData["homeTeam"])
+    awayTeams = getFullNames(cleanedData["awayTeam"])
     homeAway = cleanedData["homeAway"]
-    penalty = cleanedData["penalty"]
+    penalty = getList(cleanedData["penalty"])
     startDate = cleanedData["startDate"]
     endDate = cleanedData["endDate"]
-    refs = cleanedData["refs"]
+    refs = getList(cleanedData["refs"])
 
-    homeTeams = getFullNames(homeTeams)
-    awayTeams = getFullNames(awayTeams)
-    penalty = getList(penalty)
-    refs = getList(refs)
+    Data = {"playerNames":playerNames,
+            "homeTeams": homeTeams,
+            "awayTeams": awayTeams,
+            "homeAway": homeAway,
+            "penalty": penalty,
+            "startDate": startDate,
+            "endDate": endDate,
+            "refs": refs}
 
-    return render(request, 'results.html', {'results': myForm.cleaned_data})
+    return render(request, 'results.html', Data)
 
 def getList(i):
-  return re.split("\s*,\s*", i)
+  values = re.split("\s*,\s*", i)
+  values = [str(i) for i in values]
+  return values
 
 def getFullNames(i):
-  #Insert dictionary.
+  teamList = {  "ANA":"Anaheim Ducks",
+                "ARI":"Arizona Coyotes",
+                "BOS":"Boston Bruins",
+                "BUF":"Buffalo Sabres",
+                "CGY":"Calgary Flames",
+                "CAR":"Carolina Hurricanes",
+                "CHI":"Chicago Blackhawks",
+                "COL":"Colorado Avalanche",
+                "CBJ":"Columbus Blue Jackets",
+                "DAL":"Dallas Stars",
+                "DET":"Detroit Red Wings",
+                "EDM":"Edmonton Oilers",
+                "FLA":"Florida Panthers",
+                "LAK":"Los Angeles Kings",
+                "MIN":"Minnesota Wild",
+                "MTL":"Montreal Canadiens",
+                "NSH":"Nashville Predators",
+                "NJD":"New Jersey Devils",
+                "NYI":"New York Islanders",
+                "NYR":"New York Rangers",
+                "OTT":"Ottawa Senators",
+                "PHI":"Philadelphia Flyers",
+                "PIT":"Pittsburgh Penguins",
+                "SJS":"San Jose Sharks",
+                "STL":"St Louis Blues",
+                "TBL":"Tampa Bay Lightning",
+                "TOR":"Toronto Maple Leafs",
+                "VAN":"Vancouver Canucks",
+                "WSH":"Washington Capitals",
+                "WPG":"Winnipeg Jets"}
+  returnList = []
+
+  for entry in i:
+    returnList.append(teamList[entry])
+  return returnList
