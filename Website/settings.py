54248a3db984a73d63f4f099a7cb61c558b6e6ed
Roy Gero <roy.gero.ii@gmail.com>
2016-10-12 16:50:39 -0400

    Revert "Updating the settings for the DigitalOcean file"
    This reverts commit 1ea3727ba80d60e5131898293cf46f34ab9ec1c0.

--- Website/settings.py
+++ Website/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'Website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'roymondnet',
-	'USER': 'roymondadmin',
-	'PASSWORD': 'Colorado!91',
-	'HOST': 'localhost',
-	'POST': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -124,4 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
