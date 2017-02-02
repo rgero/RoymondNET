db15d624c3551d063d6631de62a00d6c3ceef827
Roy Gero II <roy.gero.ii@gmail.com>
2017-02-02 15:44:14 -0500

    Updating the template so it does a for loop. I’d love to make the array I’m appending to the chordList into a custom object.

--- GuitarTrainer/views.py
+++ GuitarTrainer/views.py
@@ -15,9 +15,23 @@ def index(request):
     return comingsoon(request, renderDic)
 
 def index2(request):
+
+    chordList = []
+    chordList.append( ["A", "guitartrainer\chords\A.png"] )
+    chordList.append( ["B", "guitartrainer\chords\B.png"] )
+    chordList.append( ["C", "guitartrainer\chords\C.png"] )
+    chordList.append( ["D", "guitartrainer\chords\D.png"] )
+    chordList.append( ["E", "guitartrainer\chords\E.png"] )
+    chordList.append( ["F", "guitartrainer\chords\F.png"] )
+    chordList.append( ["G", "guitartrainer\chords\G.png"] )
+
+
+
+
     renderDic = {
         'pageTitle':"The Guitar Trainer - Roymond.NET",
         'jumbotron':"The Guitar Trainer",
         'active':'None',
+        'chordList':chordList
     }
     return render(request, 'guitartrainer/index.html', renderDic)
