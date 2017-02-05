a4d47a8a94a0792b4f2c0c169c7c2dbfdd7007e2
Roy Gero II <roy.gero.ii@gmail.com>
2017-02-05 00:16:59 -0500

    Updating the CSS so that you can actually see the selected items

--- GuitarTrainer/views.py
+++ GuitarTrainer/views.py
@@ -17,24 +17,17 @@ def index(request):
 
 def index2(request):
 
-    chordList = []
-    chordList.append( ["A", "guitartrainer\chords\A.png"] )
-    chordList.append( ["B", "guitartrainer\chords\B.png"] )
-    chordList.append( ["C", "guitartrainer\chords\C.png"] )
-    chordList.append( ["D", "guitartrainer\chords\D.png"] )
-    chordList.append( ["E", "guitartrainer\chords\E.png"] )
-    chordList.append( ["F", "guitartrainer\chords\F.png"] )
-    chordList.append( ["G", "guitartrainer\chords\G.png"] )
-
-
-    formData = GuitarTrainerSetup()
-
-
+    if request.method == 'POST':
+        formData = GuitarTrainerSetup(request.POST)
+        if formData.is_valid():
+            cleanedData = formData.cleaned_data
+            print cleanedData
+    else:
+        formData = GuitarTrainerSetup()
     renderDic = {
         'pageTitle':"The Guitar Trainer - Roymond.NET",
         'jumbotron':"The Guitar Trainer",
         'active':'None',
-        'chordList':chordList,
         'form': formData
     }
     return render(request, 'guitartrainer/index.html', renderDic)
