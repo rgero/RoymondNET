828380e6eb7a2a9e0f1c1916c642b7700a377852
Roy Gero II <roy.gero.ii@gmail.com>
2017-02-28 12:02:48 -0500

    Changed the about url so it doesn’t require the HTML part at the end of it.

--- personal/urls.py
+++ personal/urls.py
@@ -18,7 +18,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^about.html', views.about, name="about"),
+    url(r'^about$', views.about, name="about")
 ]
 
 # Personal is the main page.
